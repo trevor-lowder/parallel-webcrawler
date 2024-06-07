494ab1fcaa37b47fd368d5de92f867c5e3c73086
trevor-lowder <lowder.trevor@yahoo.com>
2024-06-07 14:50:08 -0700

    Step 5 - Functional Word Counter Stream Implementation

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/WordCounts.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/WordCounts.java
@@ -4,12 +4,11 @@ import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.PriorityQueue;
+import java.util.stream.Collectors;
 
 /**
  * Utility class that sorts the map of word counts.
  *
- * <p>TODO: Reimplement the sort() method using only the Stream API and lambdas and/or method
- *          references.
  */
 final class WordCounts {
 
@@ -18,26 +17,21 @@ final class WordCounts {
    * to the provided {@link WordCountComparator}, and includes only the top
    * {@param popluarWordCount} words and counts.
    *
-   * <p>TODO: Reimplement this method using only the Stream API and lambdas and/or method
-   *          references.
    *
    * @param wordCounts       the unsorted map of word counts.
    * @param popularWordCount the number of popular words to include in the result map.
    * @return a map containing the top {@param popularWordCount} words and counts in the right order.
    */
   static Map<String, Integer> sort(Map<String, Integer> wordCounts, int popularWordCount) {
-
-    // TODO: Reimplement this method using only the Stream API and lambdas and/or method references.
-
-    PriorityQueue<Map.Entry<String, Integer>> sortedCounts =
-        new PriorityQueue<>(wordCounts.size(), new WordCountComparator());
-    sortedCounts.addAll(wordCounts.entrySet());
-    Map<String, Integer> topCounts = new LinkedHashMap<>();
-    for (int i = 0; i < Math.min(popularWordCount, wordCounts.size()); i++) {
-      Map.Entry<String, Integer> entry = sortedCounts.poll();
-      topCounts.put(entry.getKey(), entry.getValue());
-    }
-    return topCounts;
+    return wordCounts.entrySet().stream()
+      .sorted(new WordCountComparator())
+      .limit(popularWordCount)
+      .collect(Collectors.toMap(
+        Map.Entry::getKey,
+        Map.Entry::getValue,
+        (e1, e2) -> e1,
+        LinkedHashMap::new
+      ));
   }
 
   /**
