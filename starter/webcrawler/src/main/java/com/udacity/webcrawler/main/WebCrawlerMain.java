ec3592f2602caf254dd603030737648128511281
trevor-lowder <lowder.trevor@yahoo.com>
2024-06-06 19:56:15 -0700

    Step 3 - Run Legacy Crawler

--- starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/main/WebCrawlerMain.java
@@ -11,10 +11,12 @@ import com.udacity.webcrawler.profiler.Profiler;
 import com.udacity.webcrawler.profiler.ProfilerModule;
 
 import javax.inject.Inject;
+
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 public final class WebCrawlerMain {
@@ -36,7 +38,15 @@ public final class WebCrawlerMain {
 
     CrawlResult result = crawler.crawl(config.getStartPages());
     CrawlResultWriter resultWriter = new CrawlResultWriter(result);
-    // TODO: Write the crawl results to a JSON file (or System.out if the file name is empty)
+    // Write the crawl results to a JSON file (or System.out if the file name is empty)
+    String resultPath = config.getResultPath();
+    if (!resultPath.isEmpty()) {
+      Path path = Paths.get(resultPath);
+      resultWriter.write(path);
+    } else {
+      Writer out = new BufferedWriter(new OutputStreamWriter(System.out));
+      resultWriter.write(out);
+    }
     // TODO: Write the profile data to a text file (or System.out if the file name is empty)
   }
 
